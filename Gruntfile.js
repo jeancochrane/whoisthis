3bdb9265a095a1e4581d971e705b4a1ac56bdcb8
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2016-03-13 12:53:17 -0400

    node commit
    initial grunt stuff

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt){
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+	grunt.initConfig({
+		pkg:grunt.file.readJSON('package.json'),
+ 
+		watch:{
+			options:{
+				livereload: true
+			},
+			files:['js/**','css/**','*.html','json/**','themes/**'],
+		},
+  		express:{
+  			all:{
+  				options:{
+  					port: 3000,
+  					hostname:'localhost',
+  					bases:['.'],
+  					livereload: true	
+  				}
+  			}
+  		},
+		open: {
+		    all: {
+		        path: 'http://localhost:3000/index.html'
+		    }
+		}
+	});
+
+	grunt.registerTask('default',['express','open', 'watch']);
+ 
+};
